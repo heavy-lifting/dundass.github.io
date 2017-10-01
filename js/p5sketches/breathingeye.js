7d719288624b4d5dc161b47b5097ce9c6cf5baaa
dundass <lauriej1316@gmail.com>
2017-10-01 14:23:28 +0100

    width -> displayWidth ?

--- js/p5sketches/breathingeye.js
+++ js/p5sketches/breathingeye.js
@@ -6,11 +6,11 @@ function BreathingEye() {
 BreathingEye.prototype.render = function() {
   // 1536 * 769
   var t = millis() * 0.0005;
-  var sin1 = (width / 10) * Math.sin(t - 90) + (width / 10);
-  var sin2 = (width / 15) * Math.sin(t + 160) + (width / 15);
+  var sin1 = (displayWidth / 10) * Math.sin(t - 90) + (displayWidth / 10);
+  var sin2 = (displayWidth / 15) * Math.sin(t + 160) + (displayWidth / 15);
 
   push();
-  translate(width / 2, height / 2);
+  translate(displayWidth / 2, height / 2);
   noStroke();
   //fill(255,10);
   //rect(0,0,1000,1000);
@@ -22,11 +22,11 @@ BreathingEye.prototype.render = function() {
   fill(sin1, sin2, 255 - sin1, 100);
   ellipse(sin2 + 20, sin1, 10, sin1);
 
-  if(sin1 > (width / 10)) {
+  if(sin1 > (displayWidth / 10)) {
     fill(sin1, sin2, 255 - sin1, 100);
-    ellipse(sin2 + (width / 20), sin1, (sin1 - (width / 10)) * 0.66, (sin1 - (width / 10)) * 0.66);
+    ellipse(sin2 + (displayWidth / 20), sin1, (sin1 - (displayWidth / 10)) * 0.66, (sin1 - (displayWidth / 10)) * 0.66);
     fill(0, 10);
-    rect(0, 0, width, height);
+    rect(0, 0, displayWidth, height);
   }
   pop();
 
