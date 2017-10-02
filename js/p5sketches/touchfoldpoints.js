9f43e0e708ea105627776bcd87a15788ae02136e
dundass <lauriej1316@gmail.com>
2017-10-02 15:38:47 +0100

    mouse mapped to sketch centre

--- js/p5sketches/touchfoldpoints.js
+++ js/p5sketches/touchfoldpoints.js
@@ -1,6 +1,5 @@
-function ScanFoldPoints() {
+function TouchFoldPoints() {
   this.loc = 0; // loc of center
-  this.tar = 0; // target loc of center
   this.p = []; // point locs array
   this.trig1 = 0;    //0 = sin, 1 = cos, 2 = tan, 3 = atan
   this.trig2 = 1;
@@ -11,7 +10,7 @@ function ScanFoldPoints() {
   this.t_mult2 = 0.04;
 }
 
-ScanFoldPoints.prototype.update = function() {
+TouchFoldPoints.prototype.update = function() {
 	var d = 0, trig = 0, a = 0;
 	for(var i = 0; i < this.p.length; i++) {
 		for(var j = 0; j < this.p.length; j++) {
@@ -39,32 +38,20 @@ ScanFoldPoints.prototype.update = function() {
 }
 
 
-ScanFoldPoints.prototype.initRand = function() {
+TouchFoldPoints.prototype.initRand = function() {
 	randomSeed(1);
 	for(var i = 0; i < this.p.length; i++) {
 		this.p[i].x = this.loc.x + random(-50, 50);  this.p[i].y = this.loc.y + random(-250, 250);
 	}
 }
 
-ScanFoldPoints.prototype.initHorizontal = function() {
+TouchFoldPoints.prototype.initHorizontal = function() {
 	for(var i = 0; i < this.p.length; i++) {
 		this.p[i].x = 400 + (-i * i);  this.p[i].y = -150 + (frameCount % 300) + sq(1);
 	}
 }
 
-ScanFoldPoints.prototype.initMotion = function() {
-	randomSeed(frameCount % 10000);
-
-	this.loc = createVector(random(-width / 2, width / 2), random(-height / 2, -height / 4));
-	this.tar = createVector(this.loc.x * -1, random(height / 4, height / 2));
-
-	if(Math.random() > 0.5) {
-		this.loc.y *= -1;
-		this.tar.y *= -1;
-	}
-}
-
-ScanFoldPoints.prototype.randomiseTrigMappings = function() {
+TouchFoldPoints.prototype.randomiseTrigMappings = function() {
   // assigns a new non-linear coordinate behaviour
 	this.trig1 = int(Math.random() * 4); // used to be (int)random(4) in ScanFoldPoints.java
 	this.trig2 = int(Math.random() * 4);
@@ -72,7 +59,7 @@ ScanFoldPoints.prototype.randomiseTrigMappings = function() {
 	this.t_mult2 = 0.02 + (Math.random() * 0.06);
 }
 
-ScanFoldPoints.prototype.drawParticles = function(index) {
+TouchFoldPoints.prototype.drawParticles = function(index) {
 	push();
   noiseSeed(99);
 	translate(width / 2, height / 2);
@@ -85,12 +72,11 @@ ScanFoldPoints.prototype.drawParticles = function(index) {
 	pop();
 }
 
-ScanFoldPoints.prototype.render = function() {
+TouchFoldPoints.prototype.render = function() {
   background(0);
-	this.loc.x = lerp(this.loc.x, this.tar.x, 0.01);
-	this.loc.y = lerp(this.loc.y, this.tar.y, 0.01);
-	if(dist(this.loc.x, this.loc.y, this.tar.x, this.tar.y) < 2.) {
-		this.initMotion();
+	this.loc.x = mouseX - width / 2;
+	this.loc.y = mouseY - height / 2;
+	if(frameCount % 150 == 0) {
 		this.reloadCount++;
 		if(this.reloadCount % 3 == 0) this.randomiseTrigMappings();
 	}
@@ -101,10 +87,8 @@ ScanFoldPoints.prototype.render = function() {
 	}
 }
 
-ScanFoldPoints.prototype.initi = function() {
+TouchFoldPoints.prototype.initi = function() {
   this.loc = createVector(0, 0);
-  this.tar = createVector(0, 0);
-	this.initMotion();
   for(var i = 0; i < 40; i++) {
     this.p[i] = createVector(-150 + (Math.random() * 300), -150 + (Math.random() * 300));
   }
