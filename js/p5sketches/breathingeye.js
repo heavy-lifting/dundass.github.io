7f043bd79f27293b2c28f02cf671c83473071204
dundass <lauriej1316@gmail.com>
2017-10-02 16:57:28 +0100

    page per sketch

--- js/p5sketches/breathingeye.js
+++ js/p5sketches/breathingeye.js
@@ -6,27 +6,25 @@ function BreathingEye() {
 BreathingEye.prototype.render = function() {
   // 1536 * 769
   var t = millis() * 0.0005;
-  var sin1 = (displayWidth / 10) * Math.sin(t - 90) + (displayWidth / 10);
-  var sin2 = (displayWidth / 15) * Math.sin(t + 160) + (displayWidth / 15);
+  var sin1 = (width / 10) * sin(t - 90) + (width / 10);
+  var sin2 = (width / 15) * sin(t + 160) + (width / 15);
+
+  if(mouseIsPressed) {
+    this.rotateFactor = 50.0 + mouseY / 5.0;
+  }
 
   push();
-  translate(displayWidth / 2, height / 2);
+  translate(width / 2, height / 2);
   noStroke();
-  //fill(255,10);
-  //rect(0,0,1000,1000);
   rotate(t * this.rotateFactor);
-  // if(mouseIsPressed) {
-  //   fill(255, 30, 30, 50);
-  //   ellipse(mouseX, 50, 100, 100);
-  // }
   fill(sin1, sin2, 255 - sin1, 100);
   ellipse(sin2 + 20, sin1, 10, sin1);
 
-  if(sin1 > (displayWidth / 10)) {
+  if(sin1 > (width / 10)) {
     fill(sin1, sin2, 255 - sin1, 100);
-    ellipse(sin2 + (displayWidth / 20), sin1, (sin1 - (displayWidth / 10)) * 0.66, (sin1 - (displayWidth / 10)) * 0.66);
+    ellipse(sin2 + (width / 20), sin1, (sin1 - (width / 10)) * 0.66, (sin1 - (width / 10)) * 0.66);
     fill(0, 10);
-    rect(0, 0, displayWidth, height);
+    rect(0, 0, width, height);
   }
   pop();
 
