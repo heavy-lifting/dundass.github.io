1b54c8250e397d14bc8c54eb8105842bcbd791f8
dundass <lauriej1316@gmail.com>
2017-11-20 16:11:53 +0000

    engine env prep

---
+++ js/p5sketches/cucumber.js
@@ -0,0 +1,13 @@
+function Cucumber() {
+  this.slices = [];
+  this.loc = new p5.Vector(0, 0);
+  this.rot = 0;
+}
+
+Cucumber.prototype = {
+  render: function() {
+    for(var i = 0; i < slices.length; i++) {
+      
+    }
+  }
+}
